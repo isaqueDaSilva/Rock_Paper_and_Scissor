c81aa2594745f30ba0f412a2cd30aa5e8aeeccc9
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-06-25 21:46:00 -0300

    Rock, Paper and Scisors

---
+++ Rock, Paper and Scisors/Rock, Paper and Scisors/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rock, Paper and Scisors
+//
+//  Created by Isaque da Silva on 25/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rock, Paper and Scisors/Rock, Paper and Scisors/Rock__Paper_and_ScisorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock__Paper_and_ScisorsApp.swift
+//  Rock, Paper and Scisors
+//
+//  Created by Isaque da Silva on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Rock__Paper_and_ScisorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
