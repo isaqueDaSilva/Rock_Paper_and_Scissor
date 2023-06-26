c1db5af5ba80749deef731526e896fc41a77787b
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-06-25 23:04:14 -0300

    RPS 1.0

--- Rock, Paper and Scisors/Rock, Paper and Scisors/ContentView.swift
+++ Rock, Paper and Scisors/Rock, Paper and Scisors/ContentView.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var possibleMovements = ["✊", "🖐️", "✌️"]
+    @State private var finalResultOfPlayer: PossibleResults = .win
+    @State private var finalResultOfMachine: PossibleResults = .lose
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Text("Vs")
+                    .frame(maxWidth: .infinity)
+                    .font(.title.bold())
+                    .foregroundColor(.black)
+                    .background(Rectangle())
+                    .foregroundColor(.gray)
+            }
         }
-        .padding()
     }
 }
 
+enum PossibleResults: String, CaseIterable {
+    case win, lose
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
