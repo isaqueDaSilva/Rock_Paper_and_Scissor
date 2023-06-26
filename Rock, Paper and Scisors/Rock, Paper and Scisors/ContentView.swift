190ad4a41920d8dd171ae7f92d310f3ed6d7e813
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-06-25 23:39:29 -0300

    RPS 1.1

--- Rock, Paper and Scisors/Rock, Paper and Scisors/ContentView.swift
+++ Rock, Paper and Scisors/Rock, Paper and Scisors/ContentView.swift
@@ -7,15 +7,48 @@
 
 import SwiftUI
 
+struct GameSection: View {
+    
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
 struct ContentView: View {
     
-    @State private var possibleMovements = ["✊", "🖐️", "✌️"]
-    @State private var finalResultOfPlayer: PossibleResults = .win
-    @State private var finalResultOfMachine: PossibleResults = .lose
+    @State private var possibleMovements = "✊"
+//    @State private var finalResultOfPlayer: PossibleResults = .win
+//    @State private var finalResultOfMachine: PossibleResults = .lose
+    @State private var nameOfPlayer = ""
+    
+    var movements = ["✊", "🖐️", "✌️"]
+    
+    var possibleResult: PossibleResults {
+        var result: PossibleResults = .win
+        
+        // chance to tie
+        if possibleMovements == "✊" && possibleMovements == "✊" {
+            result = .tie
+        } else if possibleMovements == "🖐️" && possibleMovements == "🖐️" {
+            result = .tie
+        } else if possibleMovements == "✌️" && possibleMovements == "✌️" {
+            result = .tie
+        }
+        
+        // chance to win
+        
+        if possibleMovements == "✊" && possibleMovements == "✌️" {
+            result = .win
+        }
+        return result
+    }
     
     var body: some View {
         ZStack {
             VStack {
+                Text("")
                 Text("Vs")
                     .frame(maxWidth: .infinity)
                     .font(.title.bold())
@@ -23,12 +56,23 @@ struct ContentView: View {
                     .background(Rectangle())
                     .foregroundColor(.gray)
             }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 20)
+            .background(.thinMaterial)
+            .cornerRadius(20)
+            .shadow(radius: 10)
         }
     }
 }
 
 enum PossibleResults: String, CaseIterable {
-    case win, lose
+    case win, lose, tie
+}
+
+enum PossibleMoviments: String, CaseIterable {
+    case rock = "✊"
+    case paper = "🖐️"
+    case scissor = "✌️"
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
