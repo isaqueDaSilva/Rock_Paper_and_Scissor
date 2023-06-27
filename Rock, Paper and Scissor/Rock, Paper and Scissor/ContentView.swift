9da3d4d104ef35530ea7a4c1fe4cf3fe57efb23e
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2023-06-26 21:29:39 -0300

    RPS 2.1

--- Rock, Paper and Scissor/Rock, Paper and Scissor/ContentView.swift
+++ Rock, Paper and Scissor/Rock, Paper and Scissor/ContentView.swift
@@ -8,6 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var possibleMovementsOfPlayer: Movements = .paper
+    @State private var possibleMovimentsOfMachine: Movements = .rock
+    
+    var result: Results {
+        var result: Results = .win
+        
+        // chance of tie
+        if possibleMovementsOfPlayer == .rock && possibleMovimentsOfMachine == .rock {
+            result = .tie
+        } else if possibleMovementsOfPlayer == .paper && possibleMovimentsOfMachine == .paper {
+            result = .tie
+        } else if possibleMovementsOfPlayer == .scissor && possibleMovimentsOfMachine == .scissor {
+            result = .tie
+        }
+        
+        // chance of player win
+        if possibleMovementsOfPlayer == .rock && possibleMovimentsOfMachine == .scissor {
+            result = .win
+        } else if possibleMovementsOfPlayer == .paper && possibleMovimentsOfMachine == .rock {
+            result = .win
+        } else if possibleMovementsOfPlayer == .scissor && possibleMovimentsOfMachine == .paper {
+            result = .win
+        }
+        
+        // chance of player lose
+        if possibleMovementsOfPlayer == .rock && possibleMovimentsOfMachine == .paper {
+            result = .lose
+        } else if possibleMovementsOfPlayer == .paper && possibleMovimentsOfMachine == .scissor {
+            result = .lose
+        } else if possibleMovementsOfPlayer == .scissor && possibleMovimentsOfMachine == .rock {
+            result = .lose
+        }
+        
+        // chance of machine win
+        
+        // chance of machine lose
+        
+        return result
+    }
     var body: some View {
         VStack {
             Image(systemName: "globe")
